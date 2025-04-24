10e93b9420f47b4f8d8f84c5db1dd7ff7e07840a
Abhishek <hp7320797@gmail.com>
2025-04-24 17:28:05 +0545

    additional folder

--- users/forms.py
+++ users/forms.py
@@ -11,17 +11,100 @@ class UserSignupForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'full_name', 'phone_number', 'password1', 'password2')
+        fields = ('username', 'email', 'full_name', 'phone_number', 'password1')
 
     def clean_phone_number(self):
         phone = self.cleaned_data['phone_number']
         if not phone.isdigit() or len(phone) < 10:
             raise forms.ValidationError("Enter a valid phone number (at least 10 digits).")
         return phone
+    
+
+# List of all 77 districts of Nepal
+DISTRICT_CHOICES = [
+    ('Bhojpur', 'Bhojpur'),
+    ('Dhankuta', 'Dhankuta'),
+    ('Ilam', 'Ilam'),
+    ('Jhapa', 'Jhapa'),
+    ('Khotang', 'Khotang'),
+    ('Morang', 'Morang'),
+    ('Okhaldhunga', 'Okhaldhunga'),
+    ('Panchthar', 'Panchthar'),
+    ('Sankhuwasabha', 'Sankhuwasabha'),
+    ('Solukhumbu', 'Solukhumbu'),
+    ('Sunsari', 'Sunsari'),
+    ('Taplejung', 'Taplejung'),
+    ('Terhathum', 'Terhathum'),
+    ('Udayapur', 'Udayapur'),
+    ('Bara', 'Bara'),
+    ('Parsa', 'Parsa'),
+    ('Rautahat', 'Rautahat'),
+    ('Saptari', 'Saptari'),
+    ('Sarlahi', 'Sarlahi'),
+    ('Siraha', 'Siraha'),
+    ('Dhanusha', 'Dhanusha'),
+    ('Mahottari', 'Mahottari'),
+    ('Bhaktapur', 'Bhaktapur'),
+    ('Kathmandu', 'Kathmandu'),
+    ('Lalitpur', 'Lalitpur'),
+    ('Chitwan', 'Chitwan'),
+    ('Makwanpur', 'Makwanpur'),
+    ('Ramechhap', 'Ramechhap'),
+    ('Dolakha', 'Dolakha'),
+    ('Sindhuli', 'Sindhuli'),
+    ('Sindhupalchok', 'Sindhupalchok'),
+    ('Kavrepalanchok', 'Kavrepalanchok'),
+    ('Nuwakot', 'Nuwakot'),
+    ('Rasuwa', 'Rasuwa'),
+    ('Dhading', 'Dhading'),
+    ('Gorkha', 'Gorkha'),
+    ('Kaski', 'Kaski'),
+    ('Lamjung', 'Lamjung'),
+    ('Manang', 'Manang'),
+    ('Mustang', 'Mustang'),
+    ('Myagdi', 'Myagdi'),
+    ('Nawalpur', 'Nawalpur'),
+    ('Parbat', 'Parbat'),
+    ('Syangja', 'Syangja'),
+    ('Tanahun', 'Tanahun'),
+    ('Baglung', 'Baglung'),
+    ('Kapilvastu', 'Kapilvastu'),
+    ('Palpa', 'Palpa'),
+    ('Rupandehi', 'Rupandehi'),
+    ('Arghakhanchi', 'Arghakhanchi'),
+    ('Gulmi', 'Gulmi'),
+    ('Dang', 'Dang'),
+    ('Banke', 'Banke'),
+    ('Bardiya', 'Bardiya'),
+    ('Dailekh', 'Dailekh'),
+    ('Dolpa', 'Dolpa'),
+    ('Humla', 'Humla'),
+    ('Jajarkot', 'Jajarkot'),
+    ('Jumla', 'Jumla'),
+    ('Kalikot', 'Kalikot'),
+    ('Mugu', 'Mugu'),
+    ('Rukum East', 'Rukum East'),
+    ('Rukum West', 'Rukum West'),
+    ('Salyan', 'Salyan'),
+    ('Surkhet', 'Surkhet'),
+    ('Achham', 'Achham'),
+    ('Baitadi', 'Baitadi'),
+    ('Bajhang', 'Bajhang'),
+    ('Bajura', 'Bajura'),
+    ('Dadeldhura', 'Dadeldhura'),
+    ('Darchula', 'Darchula'),
+    ('Doti', 'Doti'),
+    ('Kanchanpur', 'Kanchanpur'),
+    ('Kailali', 'Kailali'),
+]
 
 class ManpowerSignupForm(UserCreationForm):
     name = forms.CharField(max_length=100, required=True)
-    address = forms.CharField(widget=forms.Textarea, required=True)
+    district = forms.ChoiceField(choices=DISTRICT_CHOICES, required=True, label="District")
+    local_address = forms.CharField(max_length=100, required=True, label="Local Address", widget=forms.TextInput(attrs={
+        'placeholder': 'e.g. Ward No. 5, Budhanilkantha',
+        'class': 'form-control form-control-sm'
+    }))
     phone = forms.CharField(max_length=15, required=True)
     skill = forms.CharField(max_length=100, required=True)
     experience_years = forms.IntegerField(min_value=0, required=True)
@@ -32,8 +115,12 @@ class ManpowerSignupForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'name', 'address', 'phone', 'skill', 'experience_years', 
-                  'photo', 'citizenship_front', 'citizenship_back', 'password1', 'password2')
+        fields = (
+            'username', 'email', 'name', 'district', 'local_address',
+            'phone', 'skill', 'experience_years',
+            'photo', 'citizenship_front', 'citizenship_back',
+            
+        )
 
     def clean_phone(self):
         phone = self.cleaned_data['phone']
