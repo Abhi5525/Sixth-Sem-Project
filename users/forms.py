c2fe24227c2fe40caae7c91f2005302de980f36c
Abhishek <hp7320797@gmail.com>
2025-04-26 21:45:22 +0545

    changes to the login page, the professional signup has been removed for now, and wull be added later.

--- users/forms.py
+++ users/forms.py
@@ -1,17 +1,29 @@
 # users/forms.py
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from .models import UserProfile, ManpowerProfile
+from django.contrib.auth import get_user_model, authenticate
 
 class UserSignupForm(UserCreationForm):
-    full_name = forms.CharField(max_length=100, required=True)
-    phone_number = forms.CharField(max_length=15, required=True)
-    email = forms.EmailField(required=True)
+    username = None
+    full_name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Full Name'}))
+
+    phone_number = forms.CharField(max_length=10, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Phone Number'}))
+
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'form-control','placeholder': 'Email Address'
+    }))
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'full_name', 'phone_number', 'password1')
+        fields = ('full_name', 'email', 'phone_number', 'password1', 'password2')
+
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'class':'form-control', 'placeholder':'Password'})
+        self.fields['password2'].widget.attrs.update({'class':'form-control', 'placeholder':'Confirm Password'})
+
 
     def clean_phone_number(self):
         phone = self.cleaned_data['phone_number']
@@ -19,111 +31,141 @@ class UserSignupForm(UserCreationForm):
             raise forms.ValidationError("Enter a valid phone number (at least 10 digits).")
         return phone
     
-
-# List of all 77 districts of Nepal
-DISTRICT_CHOICES = [
-    ('Bhojpur', 'Bhojpur'),
-    ('Dhankuta', 'Dhankuta'),
-    ('Ilam', 'Ilam'),
-    ('Jhapa', 'Jhapa'),
-    ('Khotang', 'Khotang'),
-    ('Morang', 'Morang'),
-    ('Okhaldhunga', 'Okhaldhunga'),
-    ('Panchthar', 'Panchthar'),
-    ('Sankhuwasabha', 'Sankhuwasabha'),
-    ('Solukhumbu', 'Solukhumbu'),
-    ('Sunsari', 'Sunsari'),
-    ('Taplejung', 'Taplejung'),
-    ('Terhathum', 'Terhathum'),
-    ('Udayapur', 'Udayapur'),
-    ('Bara', 'Bara'),
-    ('Parsa', 'Parsa'),
-    ('Rautahat', 'Rautahat'),
-    ('Saptari', 'Saptari'),
-    ('Sarlahi', 'Sarlahi'),
-    ('Siraha', 'Siraha'),
-    ('Dhanusha', 'Dhanusha'),
-    ('Mahottari', 'Mahottari'),
-    ('Bhaktapur', 'Bhaktapur'),
-    ('Kathmandu', 'Kathmandu'),
-    ('Lalitpur', 'Lalitpur'),
-    ('Chitwan', 'Chitwan'),
-    ('Makwanpur', 'Makwanpur'),
-    ('Ramechhap', 'Ramechhap'),
-    ('Dolakha', 'Dolakha'),
-    ('Sindhuli', 'Sindhuli'),
-    ('Sindhupalchok', 'Sindhupalchok'),
-    ('Kavrepalanchok', 'Kavrepalanchok'),
-    ('Nuwakot', 'Nuwakot'),
-    ('Rasuwa', 'Rasuwa'),
-    ('Dhading', 'Dhading'),
-    ('Gorkha', 'Gorkha'),
-    ('Kaski', 'Kaski'),
-    ('Lamjung', 'Lamjung'),
-    ('Manang', 'Manang'),
-    ('Mustang', 'Mustang'),
-    ('Myagdi', 'Myagdi'),
-    ('Nawalpur', 'Nawalpur'),
-    ('Parbat', 'Parbat'),
-    ('Syangja', 'Syangja'),
-    ('Tanahun', 'Tanahun'),
-    ('Baglung', 'Baglung'),
-    ('Kapilvastu', 'Kapilvastu'),
-    ('Palpa', 'Palpa'),
-    ('Rupandehi', 'Rupandehi'),
-    ('Arghakhanchi', 'Arghakhanchi'),
-    ('Gulmi', 'Gulmi'),
-    ('Dang', 'Dang'),
-    ('Banke', 'Banke'),
-    ('Bardiya', 'Bardiya'),
-    ('Dailekh', 'Dailekh'),
-    ('Dolpa', 'Dolpa'),
-    ('Humla', 'Humla'),
-    ('Jajarkot', 'Jajarkot'),
-    ('Jumla', 'Jumla'),
-    ('Kalikot', 'Kalikot'),
-    ('Mugu', 'Mugu'),
-    ('Rukum East', 'Rukum East'),
-    ('Rukum West', 'Rukum West'),
-    ('Salyan', 'Salyan'),
-    ('Surkhet', 'Surkhet'),
-    ('Achham', 'Achham'),
-    ('Baitadi', 'Baitadi'),
-    ('Bajhang', 'Bajhang'),
-    ('Bajura', 'Bajura'),
-    ('Dadeldhura', 'Dadeldhura'),
-    ('Darchula', 'Darchula'),
-    ('Doti', 'Doti'),
-    ('Kanchanpur', 'Kanchanpur'),
-    ('Kailali', 'Kailali'),
-]
-
-class ManpowerSignupForm(UserCreationForm):
-    name = forms.CharField(max_length=100, required=True)
-    district = forms.ChoiceField(choices=DISTRICT_CHOICES, required=True, label="District")
-    local_address = forms.CharField(max_length=100, required=True, label="Local Address", widget=forms.TextInput(attrs={
-        'placeholder': 'e.g. Ward No. 5, Budhanilkantha',
-        'class': 'form-control form-control-sm'
+User = get_user_model()
+class LoginForm(forms.Form):
+    username = None
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'form-control','placeholder': 'Email Address'
     }))
-    phone = forms.CharField(max_length=15, required=True)
-    skill = forms.CharField(max_length=100, required=True)
-    experience_years = forms.IntegerField(min_value=0, required=True)
-    photo = forms.ImageField(required=False)
-    citizenship_front = forms.ImageField(required=False)
-    citizenship_back = forms.ImageField(required=False)
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = (
-            'username', 'email', 'name', 'district', 'local_address',
-            'phone', 'skill', 'experience_years',
-            'photo', 'citizenship_front', 'citizenship_back',
+    password = forms.CharField(required=True, widget=forms.PasswordInput(attrs={
+        'class': 'form-control','placeholder': 'Password'
+    }))
+    
+    def clean(self):
+        cleaned_data =  super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+        
+        if email and password:
+            try:
+                user = User.objects.get(email = email)
+                user = authenticate(username = user.username, password = password)
+                if user is None:
+                    raise forms.ValidationError("Invalid credentials")
+                self.user = user
+            except User.DoesNotExist:
+                raise forms.ValidationError("Invalid Credentials")
             
-        )
+        return cleaned_data
 
-    def clean_phone(self):
-        phone = self.cleaned_data['phone']
-        if not phone.isdigit() or len(phone) < 10:
-            raise forms.ValidationError("Enter a valid phone number (at least 10 digits).")
-        return phone
+
+
+    
+
+# # List of all 77 districts of Nepal
+# DISTRICT_CHOICES = [
+#     ('Bhojpur', 'Bhojpur'),
+#     ('Dhankuta', 'Dhankuta'),
+#     ('Ilam', 'Ilam'),
+#     ('Jhapa', 'Jhapa'),
+#     ('Khotang', 'Khotang'),
+#     ('Morang', 'Morang'),
+#     ('Okhaldhunga', 'Okhaldhunga'),
+#     ('Panchthar', 'Panchthar'),
+#     ('Sankhuwasabha', 'Sankhuwasabha'),
+#     ('Solukhumbu', 'Solukhumbu'),
+#     ('Sunsari', 'Sunsari'),
+#     ('Taplejung', 'Taplejung'),
+#     ('Terhathum', 'Terhathum'),
+#     ('Udayapur', 'Udayapur'),
+#     ('Bara', 'Bara'),
+#     ('Parsa', 'Parsa'),
+#     ('Rautahat', 'Rautahat'),
+#     ('Saptari', 'Saptari'),
+#     ('Sarlahi', 'Sarlahi'),
+#     ('Siraha', 'Siraha'),
+#     ('Dhanusha', 'Dhanusha'),
+#     ('Mahottari', 'Mahottari'),
+#     ('Bhaktapur', 'Bhaktapur'),
+#     ('Kathmandu', 'Kathmandu'),
+#     ('Lalitpur', 'Lalitpur'),
+#     ('Chitwan', 'Chitwan'),
+#     ('Makwanpur', 'Makwanpur'),
+#     ('Ramechhap', 'Ramechhap'),
+#     ('Dolakha', 'Dolakha'),
+#     ('Sindhuli', 'Sindhuli'),
+#     ('Sindhupalchok', 'Sindhupalchok'),
+#     ('Kavrepalanchok', 'Kavrepalanchok'),
+#     ('Nuwakot', 'Nuwakot'),
+#     ('Rasuwa', 'Rasuwa'),
+#     ('Dhading', 'Dhading'),
+#     ('Gorkha', 'Gorkha'),
+#     ('Kaski', 'Kaski'),
+#     ('Lamjung', 'Lamjung'),
+#     ('Manang', 'Manang'),
+#     ('Mustang', 'Mustang'),
+#     ('Myagdi', 'Myagdi'),
+#     ('Nawalpur', 'Nawalpur'),
+#     ('Parbat', 'Parbat'),
+#     ('Syangja', 'Syangja'),
+#     ('Tanahun', 'Tanahun'),
+#     ('Baglung', 'Baglung'),
+#     ('Kapilvastu', 'Kapilvastu'),
+#     ('Palpa', 'Palpa'),
+#     ('Rupandehi', 'Rupandehi'),
+#     ('Arghakhanchi', 'Arghakhanchi'),
+#     ('Gulmi', 'Gulmi'),
+#     ('Dang', 'Dang'),
+#     ('Banke', 'Banke'),
+#     ('Bardiya', 'Bardiya'),
+#     ('Dailekh', 'Dailekh'),
+#     ('Dolpa', 'Dolpa'),
+#     ('Humla', 'Humla'),
+#     ('Jajarkot', 'Jajarkot'),
+#     ('Jumla', 'Jumla'),
+#     ('Kalikot', 'Kalikot'),
+#     ('Mugu', 'Mugu'),
+#     ('Rukum East', 'Rukum East'),
+#     ('Rukum West', 'Rukum West'),
+#     ('Salyan', 'Salyan'),
+#     ('Surkhet', 'Surkhet'),
+#     ('Achham', 'Achham'),
+#     ('Baitadi', 'Baitadi'),
+#     ('Bajhang', 'Bajhang'),
+#     ('Bajura', 'Bajura'),
+#     ('Dadeldhura', 'Dadeldhura'),
+#     ('Darchula', 'Darchula'),
+#     ('Doti', 'Doti'),
+#     ('Kanchanpur', 'Kanchanpur'),
+#     ('Kailali', 'Kailali'),
+# ]
+
+# class ManpowerSignupForm(UserCreationForm):
+#     name = forms.CharField(max_length=100, required=True)
+#     district = forms.ChoiceField(choices=DISTRICT_CHOICES, required=True, label="District")
+#     local_address = forms.CharField(max_length=100, required=True, label="Local Address", widget=forms.TextInput(attrs={
+#         'placeholder': 'e.g. Ward No. 5, Budhanilkantha',
+#         'class': 'form-control form-control-sm'
+#     }))
+#     phone = forms.CharField(max_length=15, required=True)
+#     skill = forms.CharField(max_length=100, required=True)
+#     experience_years = forms.IntegerField(min_value=0, required=True)
+#     photo = forms.ImageField(required=False)
+#     citizenship_front = forms.ImageField(required=False)
+#     citizenship_back = forms.ImageField(required=False)
+#     email = forms.EmailField(required=True)
+
+    # class Meta:
+    #     model = User
+    #     fields = (
+    #         'username', 'email', 'name', 'district', 'local_address',
+    #         'phone', 'skill', 'experience_years',
+    #         'photo', 'citizenship_front', 'citizenship_back',
+            
+    #     )
+
+    # def clean_phone(self):
+    #     phone = self.cleaned_data['phone']
+    #     if not phone.isdigit() or len(phone) < 10:
+    #         raise forms.ValidationError("Enter a valid phone number (at least 10 digits).")
+    #     return phone
--- users/views.py
+++ users/views.py
@@ -1,20 +1,19 @@
 
-
 # users/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.views import LoginView
 from django.urls import reverse
-from .forms import UserSignupForm, ManpowerSignupForm
-from .models import UserProfile, ManpowerProfile
+from .forms import UserSignupForm, LoginForm
+from .models import UserProfile
 
 def profile(request):
     return render(request, 'users/profile.html')
 
-def signup_choice(request):
-    return render(request, 'users/signup_choice.html')
+# def signup_choice(request):
+#     return render(request, 'users/signup_choice.html')
 
-def user_signup(request):
+def signup(request):
     if request.method == 'POST':
         form = UserSignupForm(request.POST)
         if form.is_valid():
@@ -27,30 +26,35 @@ def user_signup(request):
             return redirect('users:login')
     else:
         form = UserSignupForm()
-    return render(request, 'users/user_signup.html', {'form': form})
+    return render(request, 'users/signup.html', {'form': form})
 
-def professional_signup(request):
+# def professional_signup(request):
+#     if request.method == 'POST':
+#         form = ManpowerSignupForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             user = form.save()
+#             ManpowerProfile.objects.create(
+#                 user=user,
+#                 name=form.cleaned_data['name'],
+#                 address=form.cleaned_data['address'],
+#                 phone=form.cleaned_data['phone'],
+#                 skill=form.cleaned_data['skill'],
+#                 experience_years=form.cleaned_data['experience_years'],
+#                 photo=form.cleaned_data['photo'],
+#                 citizenship_front=form.cleaned_data['citizenship_front'],
+#                 citizenship_back=form.cleaned_data['citizenship_back']
+#             )
+#             return redirect('users:login')
+#     else:
+#         form = ManpowerSignupForm()
+#     return render(request, 'users/professional_signup.html', {'form': form})
+def login(request):
     if request.method == 'POST':
-        form = ManpowerSignupForm(request.POST, request.FILES)
+        form = LoginForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            ManpowerProfile.objects.create(
-                user=user,
-                name=form.cleaned_data['name'],
-                address=form.cleaned_data['address'],
-                phone=form.cleaned_data['phone'],
-                skill=form.cleaned_data['skill'],
-                experience_years=form.cleaned_data['experience_years'],
-                photo=form.cleaned_data['photo'],
-                citizenship_front=form.cleaned_data['citizenship_front'],
-                citizenship_back=form.cleaned_data['citizenship_back']
-            )
-            return redirect('users:login')
+            auth_login(request, form.user)
+            return redirect('users:profile')
+           
     else:
-        form = ManpowerSignupForm()
-    return render(request, 'users/professional_signup.html', {'form': form})
-
-class CustomLoginView(LoginView):
-    template_name = 'users/login.html'
-    def get_success_url(self):
-        return reverse('users:profile')
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form':form})
